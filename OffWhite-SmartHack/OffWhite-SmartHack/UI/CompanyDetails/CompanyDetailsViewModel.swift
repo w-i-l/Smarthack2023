f876ff7842775f21165a74641844dbc84226b2ba
Alexia Aldea <alexia.elena.aldea@gmail.com>
2023-11-12 11:46:16 +0200

    fixed bug

--- OffWhite-SmartHack/OffWhite-SmartHack/UI/CompanyDetails/CompanyDetailsViewModel.swift
+++ OffWhite-SmartHack/OffWhite-SmartHack/UI/CompanyDetails/CompanyDetailsViewModel.swift
@@ -11,10 +11,12 @@ class CompanyDetailsViewModel: ObservableObject {
     @Published var companyName: String
     @Published var revenue: String
     @Published var stackPrice: String
+    @Published var number: Double
     
-    init(companyName: String, revenue: String, stackPrice: String) {
+    init(companyName: String, revenue: String, stackPrice: String, number: Double) {
         self.companyName = companyName
         self.revenue = revenue
         self.stackPrice = stackPrice
+        self.number = number
     }
 }
